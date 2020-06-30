8dca40516345ea4947dae0a6139c219e2573f565
Samuel Knight <samueltobyknight@gmail.com>
2020-06-30 15:24:54 +0100

    package.json, server.js tidy

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const settings = {
   client_id: '49531',
   client_secret: 'cbadaecd9d626826962ddde695f7067f52eb2da2',
   mapboxToken: 'pk.eyJ1Ijoic2tzdHVkaW8iLCJhIjoiY2syMmF6cmp2MWg2eDNjbXY3am14ZzNlYyJ9.6o1_m77WQE0hY8orwGldUg',
-  meteostatToken: 'ZFi2w03vVUzXqJokEz0W4QvyjpOWkcf5',
 };
 
 app.use(express.static(`${__dirname}/src`));
-app.use(express.static('js'));
 
 app.get('/oauth2-redirect', (req, res) => {
   console.log(`${req.query} __authorised`);
